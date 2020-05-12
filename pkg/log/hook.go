a0271cd684257c8764ab7eac49c4e55bd4139119
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-05-12 19:28:21 +0100

    chore: fix coverage profile

--- pkg/log/hook.go
+++ pkg/log/hook.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	"github.com/pkg/errors"
 	"os"
 
@@ -22,13 +21,11 @@ func NewHook(path string, levels []logrus.Level) *Hook {
 func (h *Hook) Fire(entry *logrus.Entry) error {
 	b, err := h.formatter.Format(entry)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 
 	err = appendToFile(h.path, string(b))
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 	return nil
@@ -42,20 +39,18 @@ func (h *Hook) Levels() []logrus.Level {
 func appendToFile(path string, message string) error {
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 
 	defer f.Close()
 
 	if err = Append(f, []byte(message)); err != nil {
-		fmt.Println(err)
 		return err
 	}
 	return nil
 }
 
-var invalidN bool // initalized to false
+var invalidN bool // initialized to false
 
 func Append(f *os.File, data []byte) error {
 	n, err := f.Write(data)
@@ -65,6 +60,5 @@ func Append(f *os.File, data []byte) error {
 	if n != len(data) || invalidN {
 		return errors.New("failed appending")
 	}
-	// ...
 	return nil
 }
